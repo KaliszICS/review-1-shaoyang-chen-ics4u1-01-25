4082c36d651c4fd7f3262735610d2e920e8b13d5
Shaoyang Chen <shaoyang.chen@student.tdsb.on.ca>
2025-09-05 01:00:27 +0000

    updated review1

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,22 +2,35 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+		String sentenceQ1 = "There once was a man from St. Ives.";
+        System.out.println(sentenceQ1);
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		int num1Q2 = 9;
+        int num2Q2 = 5;
+        System.out.println(num1Q2/num2Q2);
+        System.out.println(num1Q2%num2Q2);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		Boolean Q4 = true;
+        System.out.println(Q4);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		String section1Q5 = "Hello";
+        String section2Q5 = "World";
+        System.out.print(section1Q5 + " ");
+        System.out.print(section2Q5);
 	}
 
 }
